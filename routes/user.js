277ba6f5b22476f79f74c1c23fc88e726022c022
Matheus Souza <matheu.sk.9@hotmail.com>
2019-03-13 22:35:57 -0300

    Handle registration

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+//const passport = require('passport')
 const router = express.Router()
+
+// Load User Model
+require('../models/User')
+const User = mongoose.model('users')
 
 // User Login Router
 router.get('/login', (req, res) => {
@@ -14,8 +20,25 @@ router.get('/register', (req, res) => {
 
 // Register Form Post
 router.post('/register', (req, res) => {
-    console.log(req.body)
-    res.send('register')
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err
+            newUser.password = hash;
+            newUser.save()
+                .then(user => {
+                    res.redirect('/user/login')
+                })
+                .catch(err => {
+                    console.log(err)
+                    return
+                })
+        })
+    })
 })
 
 module.exports = router
