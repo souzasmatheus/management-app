94ee7b3cb47ebd859a19569f28ea9a14399a01fd
Matheus Souza <matheu.sk.9@hotmail.com>
2019-03-16 19:40:24 -0300

    Handle registration with bcrypt

--- routes/user.js
+++ routes/user.js
@@ -30,13 +30,13 @@ router.post('/login', (req, res, next) => {
 router.post('/register', (req, res) => {
     let errors = []
 
-    if(req.body.password !== req.body.password2) {
+    if (req.body.password !== req.body.password2) {
         errors.push({
             text: 'As senhas devem ser iguais'
         })
     }
 
-    if(errors.length > 0) {
+    if (errors.length > 0) {
         res.render('user/register', {
             errors: errors,
             name: req.body.name,
@@ -45,29 +45,37 @@ router.post('/register', (req, res) => {
             password2: req.body.password2
         })
     } else {
-        res.send('passed')
-    }
-
-    /*
-    const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err
-            newUser.password = hash;
-            newUser.save()
-                .then(user => {
-                    res.redirect('/user/login')
-                })
-                .catch(err => {
-                    console.log(err)
-                    return
-                })
+        User.findOne({
+            email: req.body.email
         })
-    })*/
+            .then(user => {
+                if (user) {
+                    req.flash('error_msg', 'Este e-mail já está cadastrado')
+                    res.redirect('/user/login')
+                } else {
+                    const newUser = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    })
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err
+                            newUser.password = hash;
+                            newUser.save()
+                                .then(user => {
+                                    req.flash('success_msg', 'Usuário cadastrado')
+                                    res.redirect('/user/login')
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                    return
+                                })
+                        })
+                    })
+                }
+            })
+    }
 })
 
 module.exports = router
