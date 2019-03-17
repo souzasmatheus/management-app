48cc1b58fd5241462d30fc405814dd377e024cca
Matheus Souza <matheu.sk.9@hotmail.com>
2019-03-16 21:34:31 -0300

    Set user global variable and change template if user is logged in

--- app.js
+++ app.js
@@ -56,6 +56,10 @@ app.use(session({
     saveUninitialized: true
 }))
 
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Flash Middleware
 app.use(flash())
 
@@ -64,6 +68,7 @@ app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg')
     res.locals.error_msg = req.flash('error_msg')
     res.locals.error = req.flash('error')
+    res.locals.user = req.user || null
     next()
 })
 
