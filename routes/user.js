671039c4d493022fa41e12aa225207c028388845
Matheus Souza <matheu.sk.9@hotmail.com>
2019-03-20 14:07:59 -0300

    Handle password change

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const router = express.Router()
-const {ensureMasterAuthentication, ensureAuthenticated} = require('../lib/auth')
+const { ensureMasterAuthentication, ensureAuthenticated } = require('../lib/auth')
 
 // Load User Model
 require('../models/User')
@@ -83,6 +83,50 @@ router.post('/register', ensureMasterAuthentication, (req, res) => {
 // Change Password Router
 router.get('/password', ensureAuthenticated, (req, res) => {
     res.render('user/password')
+})
+
+// Handle New Password Form
+router.put('/password', ensureAuthenticated, (req, res) => {
+    let errors = []
+
+    if (req.body.password1 != req.body.password2) {
+        errors.push({
+            text: 'A nova senha e a confirmação devem ser iguais'
+        })
+    }
+
+    if (errors.length > 0) {
+        res.render('user/password', {
+            errors: errors
+        })
+    } else {
+        User.findOne({
+            _id: req.user.id
+        })
+            .then((user) => {
+                if (bcrypt.compare(req.body.old_password, user.password)) {
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(req.body.password1, salt, (err, hash) => {
+                            if (err) throw err
+                            user.password = hash;
+                            user.save()
+                                .then(user => {
+                                    req.logout()
+                                    req.flash('success_msg', 'Nova senha atualizada')
+                                    res.redirect('/user/login')
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                    return
+                                })
+                        })
+                    })
+                } else {
+                    req.flash('error_msg', 'A senha antiga está incorreta')
+                    res.redirect('/user/password')
+                }
+            })
+    }
 })
 
 // Logout User
